8e5b120220d4cde163c1c7242b579d5fa19e1f33
Mikołaj Depta <mikolajdepta2000@gmail.com>
2021-11-19 20:57:57 +0100

    Add test cases for Subtraction and Division

--- sheet05/test_expressions.py
+++ sheet05/test_expressions.py
@@ -8,7 +8,7 @@ class TestDerivative(unittest.TestCase):
     def test_const(self):
         """Derivation variable cannot be inferred so we expect a DerivationError."""
         self.assertRaises(
-            DerivationError,
+            LangDerivationError,
             lambda: ArithmeticExpression.derivative(Constant(IntValue(1)))
         )
 
@@ -19,23 +19,27 @@ class TestDerivative(unittest.TestCase):
             self.assertEqual(ArithmeticExpression.derivative(var), Constant(IntValue(1)))
 
     def test_add(self):
-        expr = Add(Variable("x"), Constant(IntValue(1)))
-        self.assertTrue(ArithmeticExpression.derivative(expr) == Add(Constant(IntValue(1)), Constant(IntValue(0))))
+        expr = Variable("x") + Constant(IntValue(1))
+        self.assertEqual(ArithmeticExpression.derivative(expr), Constant(IntValue(1)) + Constant(IntValue(0)))
+
+    def test_subtract(self):
+        expr = Variable("x") - Constant(IntValue(1))
+        self.assertEqual(ArithmeticExpression.derivative(expr), Constant(IntValue(1)) - Constant(IntValue(0)))
 
     def test_times(self):
-        expr = Times(Variable("y"), Constant(IntValue(42)))
+        expr = Variable("y") * Constant(IntValue(42))
         self.assertEqual(
             ArithmeticExpression.derivative(expr),
-            Times(
-                Times(
-                    Constant(IntValue(1)),
-                    Constant(IntValue(42))
-                    ),
-                Times(
-                    Constant(IntValue(0)),
-                    Variable("y")
-                )
-            ))
+            Constant(IntValue(1)) * Constant(IntValue(42)) + Constant(IntValue(0)) * Variable("y")
+        )
+
+    def test_divide(self):
+        expr = Variable("y") / Constant(IntValue(42))
+        self.assertEqual(
+            ArithmeticExpression.derivative(expr),
+            (Constant(IntValue(1)) * Constant(IntValue(42)) - Constant(IntValue(0)) * Variable("y"))
+            / (Constant(IntValue(42)) * Constant(IntValue(42)))
+        )
 
     def test_const_with_var(self):
         """No DerivationError because we specify the var."""
